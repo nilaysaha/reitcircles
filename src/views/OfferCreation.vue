54c351abc6e03cf8a991d2c64800696f7401238c
Asif Encoder IT <asif@encoderit.net>
2022-01-26 23:21:51 +0600

    Offer creation page tabs added.

--- src/views/OfferCreation.vue
+++ src/views/OfferCreation.vue
@@ -9,11 +9,14 @@
 		</v-tabs>
 		<v-tabs-items v-model="tabs">
 			<v-tab-item class="tw-pt-2">
+        <div>
+          <h1 class="tw-text-4xl tw-mb-2 tw-font-bold tw-text-42">Basic Information </h1>
+          <span class="tw-text-77">UNIT NUMBER</span>
+          <p class="tw-text-xl tw-mt-5 tw-text-77">what is  the number of your apartment or house?</p>
+        </div>
 				<div class="tw-mt-24">
 					<v-row>
 						<v-col cols="12" md="6" lg="6" xl="4">
-
-
 							<v-select
 								dark
 								class="tw-mt-3.5 asset"
@@ -22,19 +25,48 @@
 								solo
 								:items="['Apartment','House','Townhouse','Property']"
 							></v-select>
-							<v-col cols="12" md="6">
-								<v-text-field
-									class="mb-5 identity-proof"
-									hide-details="auto"
-									dense
-									:rules="[rules.required('First Name is required')]"
-									placeholder="First Name"
-									solo
-								></v-text-field>
-							</v-col>
-							<v-col cols="12" md="6"></v-col>
-							<v-col cols="12" md="6"></v-col>
-							<v-col cols="12" md="6"></v-col>
+							<v-row>
+								<v-col class="my-0 py-0" cols="12" md="6">
+									<v-text-field
+										class="mb-5 identity-proof"
+										hide-details="auto"
+										dense
+										:rules="[rules.required('Post Code is required')]"
+										placeholder="Post Code"
+										solo
+									></v-text-field>
+								</v-col>
+								<v-col class="my-0 py-0" cols="12" md="6">
+									<v-text-field
+										class="mb-5 identity-proof"
+										hide-details="auto"
+										dense
+										:rules="[rules.required('City is required')]"
+										placeholder="City"
+										solo
+									></v-text-field>
+								</v-col>
+								<v-col class="my-0 py-0" cols="12" md="6">
+									<v-text-field
+										class="mb-5 identity-proof"
+										hide-details="auto"
+										dense
+										:rules="[rules.required('Province is required')]"
+										placeholder="Province"
+										solo
+									></v-text-field>
+								</v-col>
+								<v-col class="my-0 py-0" cols="12" md="6">
+									<v-text-field
+										class="mb-5 identity-proof"
+										hide-details="auto"
+										dense
+										:rules="[rules.required('Country is required')]"
+										placeholder="Country"
+										solo
+									></v-text-field>
+								</v-col>
+							</v-row>
 						</v-col>
 					</v-row>
 				</div>
